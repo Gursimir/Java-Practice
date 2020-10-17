a585dfd0f1d6eb322aa02b4038130bc5e535f3a2
Gursimran kaur <64028005+Gursimir@users.noreply.github.com>
2020-10-17 18:17:38 +0530

    Loop ll

---
+++ loop_2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+class Loop_2{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int q=in.nextInt();
+        for(int i=0;i<q;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int p = 1;
+
+            for(int j=0;j<n;j++){
+                a = a + p*b;
+                System.out.printf("%d ",a);
+                /*x<<N shifts bits of x left by distance N; fills with  0 bits on right side*/
+                p = p << 1 ;
+            }
+            System.out.println();
+        }
+        in.close();
+
+    }
+}
