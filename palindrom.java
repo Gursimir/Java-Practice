ec3ecd6aa389d0611a241e5209f9916ce1152b41
Gursimran kaur <64028005+Gursimir@users.noreply.github.com>
2020-10-24 23:41:06 +0530

    Palindrome
    Reversing the string

---
+++ palindrom.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class palindrom {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        sc.close();
+
+        String reverse ="";
+        for (int i =A.length()-1;i>=0;i--){
+            reverse = reverse+A.charAt(i); 
+        }
+
+        if (reverse.equalsIgnoreCase(A)){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+        
+    }
+}
+
+
+
