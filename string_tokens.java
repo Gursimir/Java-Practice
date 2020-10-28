11605f9053b5334805fee0b3b2d6b344017f719c
Gursimran kaur <64028005+Gursimir@users.noreply.github.com>
2020-10-28 22:56:52 +0530

    String Token
    String split method

--- string_tokens.java
+++ string_tokens.java
@@ -6,7 +6,15 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
-        // Write your code here.
+        
+         String[] str = s.split(" ");
+        int l = str.length;
+
+        System.out.println("l");
+        for(int i=0;i<l;i++){
+            System.out.println(str[i]);
+        }
+        
         scan.close();
     }
 }
