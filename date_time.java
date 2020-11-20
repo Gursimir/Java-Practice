a559f64816ad1375449d800f01a5a62fef3e8045
Gursimir <kgursimran648@gmail.com>
2020-11-20 10:38:01 +0530

    Date time

--- date_time.java
+++ date_time.java
@@ -21,7 +21,7 @@ class Result {
 
 }
 
-public class Solution {
+public class date_time {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
