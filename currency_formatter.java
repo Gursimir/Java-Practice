3135e89237b721e186f185a195e67b23f744872e
Gursimir <kgursimran648@gmail.com>
2020-11-20 10:37:03 +0530

    Currency Formatter

--- currency_formatter.java
+++ currency_formatter.java
@@ -2,7 +2,7 @@
 import java.util.*;
 import java.text.*;
 
-public class Solution {
+public class currency_formatter {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
