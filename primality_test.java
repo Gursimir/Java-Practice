3f7cd881889de4cf4c23839f7bc5aa888995fb19
Gursimir <kgursimran648@gmail.com>
2020-12-01 23:17:38 +0530

    Primality Test

--- primality_test.java
+++ primality_test.java
@@ -1,20 +1,18 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class primality_test {
-
-
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String n = scanner.nextLine();
-
+        BigInteger num = scanner.nextBigInteger();
+        boolean result = num.isProbablePrime(1);
+        if(result == true){
+            System.out.println("prime");
+        }else{
+            System.out.println("not prime");
+        }
         scanner.close();
     }
 }
